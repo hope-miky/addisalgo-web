e1f7d73060d061b2763db01f31cbbef39464f936
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-01-15 16:35:54 +0300

    updated navbar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.use(router)
 Vue.config.productionTip = false
