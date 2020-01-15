26aac168d3894a0580a0e6232afee34b69ba3f0f
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-01-15 16:30:00 +0300

    added dashboard and navbar

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from "./Navbar";
+export default {
+    name: 'DashBoard',
+    components: {
+        NavBar,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
