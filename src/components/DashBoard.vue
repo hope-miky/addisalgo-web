7436c5b28a80e65dc51887129bf55825ae5ec8f1
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-01-21 10:46:36 +0300

    changed container to card in dashboard

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,28 +1,46 @@
 <template>
-  <div>
+  <div id="app">
       <NavBar />
 
-      <div class="container">
+      <div class="container col-auto m-5 p-5">
+          
     <div class="row">
-        <a class="aFordiv" href="Algorithm">
+            
         <div class="col-sm">
             <div class="container border p-5 shadow">
-                this is one
+                <div class="card-img">
+                    <img src="@/assets/logo.png">
+                </div>
+                    <div class="card-body">
+                            Algorithms
+                    </div>
+                    
             </div>
         </div>
-        </a>
+        
             <div class="col-sm">
               <div class="container border p-5 shadow">
-                this is two
+               <div class="card-img">
+                    <img src="@/assets/logo.png">
+                </div>
+                    <div class="card-body">
+                            Algorithms
+                    </div>
             </div>
         </div>
         <div class="col-sm">
            <div class="container border p-5 shadow">
-                this is three
+                <div class="card-img">
+                    <img src="@/assets/logo.png">
+                </div>
+                    <div class="card-body">
+                            Algorithms
+                    </div>
             </div>
         </div>
     </div>
     </div>
+
     </div>
 </template>
 
@@ -42,11 +60,19 @@ export default {
 </script>
 
 <style scoped>
+
 .container {
     margin: 16px;
     border-color: black;
     border-width: 10px;
     border-radius: 25px;
     box-shadow: 10cm;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 25%;
     }
+
 </style>
