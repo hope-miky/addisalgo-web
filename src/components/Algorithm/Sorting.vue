5d9c6b6e02d90b3428463e70b1250156635a3ee9
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-01-26 14:12:45 +0300

    embedded a chart for sorting tab

--- src/components/Algorithm/Sorting.vue
+++ src/components/Algorithm/Sorting.vue
@@ -1,53 +1,90 @@
 <template>
-  <div>
-      <div class=" container ">
-         <button1 v-on:click.stop="randomNum()" class="btn btn-primary" style="float: right;">Random</button1>
-      </div>
-        <pure-vue-chart
-            :points="this.points"
-            :width="500"
-            :height="300"
-            :show-y-axis="false"
-            :show-x-axis="true"
-        />  
-        </div>
+  <div class="small">
+    <button @click="fillData()" style="float: right;" class="btn btn-info">Randomize Data</button>
+    <line-chart :chart-data="datacollection"
+      :options="options"
+    ></line-chart>
+  </div>
 </template>
 
 <script>
-//import PureChart from './chart/PureChart.vue'
-import PureVueChart from 'pure-vue-chart';
-
+import LineChart from "../LineChart.js";
 export default {
+  name: 'Sorting',
     components: {
-        //PureChart
-        PureVueChart
+      LineChart
     },
-    name: 'Sorting',
-    data() {
-        return {
-            source: [1,2,3,4,5,6,7,8,9],
-        }
-    },
-    
-    props: {
-        points: { type: Array, 
-        default: () => [5,8,9,6,3,4,1,5] 
-        },
-    },
+    data () {
+      return {
+        datacollection:null,
+        options: {
+              legend: {
+                  display: false
+              },
+              scales: {
+              xAxes: [{
+                  gridLines: {
+                      offsetGridLines: true,
+                      offset: true,
 
+                  }
+              }],
+              yAxes: [{
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 50
+                }
+            }]
+          },
+              tooltips: {
+                  callbacks: {
+                    label: function(tooltipItem) {
+                            return tooltipItem.yLabel;
+                    }
+                  }
+              }
+          }
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
     methods: {
-         randomNum() {
-            for(let i = 0; i < 10; i++){
-                this.points[i] = Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-            }
-            alert(this.points)
-        },
+      fillData () {
+        
+        this.datacollection = {
+          datasets: [
+            {
+              backgroundColor: '#007079',
+              maxBarThickness: 5,
+              data: [this.getRandomInt()]
+            }, 
+          ]
+        };
 
-    
+        for (let i =0; i< 80; i++){
+          this.datacollection.datasets.push({
+            backgroundColor: '#007079',
+            
+            maxBarThickness: 5,
+            data: [this.getRandomInt()]
+          })
+        }
+        
+
+       
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (55 - 10 + 1)) + 10
+      }
     }
-}
+  }
 </script>
 
-<style scoped>
+<style>
 
+.small {
+    max-width: 600px;
+    margin:  50px auto;
+}
 </style>
